21f583dd49715c379165bb8fd3c04177b7eaae29
jaldemirbn <jaldemirbn@gmail.com>
2026-08-20 22:12:16 -0300

    Troa de endereço da minha foto

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -239,7 +239,7 @@ export default function App() {
           <div className="hidden md:flex justify-end">
             <div className="relative w-[420px] h-[480px] rounded-2xl overflow-hidden bg-muted border border-border/30">
               <img
-                src="../../minha-foto.png"
+                src={fotoPerfil}
                 alt="Front-End Developer"
                 className="w-full h-full object-cover grayscale"
               />
